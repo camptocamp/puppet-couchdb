0fe17eb00c87e05d737f89c2162519088ca858ff
Mickaël Canévet <mickael.canevet@camptocamp.com>
2012-12-06 16:51:06 +0100

    Lint couchdb::params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,17 @@
 class couchdb::params {
 
   $bind_address = $couchdb_bind_address ? {
-    ""      => "127.0.0.1",
+    ''      => '127.0.0.1',
     default => $couchdb_bind_address,
   }
 
   $port = $couchdb_port ? {
-    ""      => "5984",
+    ''      => '5984',
     default => $couchdb_port,
   }
 
   $backupdir = $couchdb_backupdir ? {
-    ""      => "/var/backups/couchdb",
+    ''      => '/var/backups/couchdb',
     default => $couchdb_backupdir,
   }
 
