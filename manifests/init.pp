aa8e03bdd337b742b9648f2acc3a62f2dc4f4a77
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2013-08-06 14:10:31 +0200

    Merge commit '1b70aeeb39fde0e6cdbc726106ff13b1e3c1b7d7' into staging

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class couchdb {
   case $::operatingsystem {
     Debian: {
       case $::lsbdistcodename {
-        /lenny|squeeze/: { include couchdb::debian }
-        default:         { fail "couchdb not available for ${::operatingsystem}/${::lsbdistcodename}" }
+        /lenny|squeeze|wheezy/: { include couchdb::debian }
+        default: { fail "couchdb not available for ${::operatingsystem}/${::lsbdistcodename}" }
       }
     }
     RedHat: { include couchdb::redhat }
