3b4ca5d68c2f5b9506eb3766dd8052863632a1d5
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-12-26 17:13:36 +0100

    Use rspec-puppet-facts

--- spec/classes/couchdb_spec.rb
+++ spec/classes/couchdb_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe 'couchdb' do
-  let(:facts) {{
-    :lsbdistcodename => 'wheezy',
-    :osfamily        => 'Debian',
-  }}
-  it { should compile.with_all_deps }
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge({
+          :couchdb_bind_address => '0.0.0.0',
+          :couchdb_port         => 5984,
+        })
+      end
+
+      it { should compile.with_all_deps }
+    end
+  end
 end
